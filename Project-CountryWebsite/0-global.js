e1c65b6d185bd9870af0785b0e60418e43cbe826
NotAuvance <aakifumar55@gmail.com>
2024-10-23 17:34:27 -0700

    Initial commit

---
+++ Project-CountryWebsite/0-global.js
@@ -0,0 +1,32 @@
+// [0] Lenis Scroll - Lenis Plugin - Smooth Scrolling
+
+
+
+
+
+
+
+
+
+
+
+
+// [2] Burger Menu Toggle (Only Mobile)
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector("#hamburger-menu");
+    const overlay = document.querySelector("#menuOverlay");
+    const menuLinks = document.querySelectorAll(".overlay-content a");
+  
+    // [3.1] Overlay Activation (w/ Burger Menu)
+    menuIcon.addEventListener("click", function () {
+        overlay.classList.toggle("active");
+    });
+  
+    // [3.2]  Reversing Overlay & Burger Animations
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            overlay.classList.remove("active"); // Hides overlay when one of the link is clicked
+            menuIcon.classList.toggle("active") // Simultaneously, the hamburger animation goes back to its original state
+        });
+    });
+  });
